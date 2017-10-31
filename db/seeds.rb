60ecae7a0f4dda71ce4bad92cf58facddf0858ea
Diogo Heinen <eltigredc@gmail.com>
2017-10-31 12:36:43 +0100

    task manager rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts "Deleting all tasks"
+Task.destroy_all
+
+
+puts "Creating 25 tasks"
+25.times do
+  task = Task.create(name: Faker::Hacker.ingverb, description:
+Faker::Hacker.say_something_smart)
+  puts "Created #{task.name}"
+end
+
+puts "Bye!"
